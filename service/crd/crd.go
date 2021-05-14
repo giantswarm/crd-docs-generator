20e6f406dfbf37d3d51ebf2859e5d7f70350838e
Marian Steinbach <marian@giantswarm.io>
2021-05-14 12:54:30 +0200

    Parse multiple CRDs from a single file

--- service/crd/crd.go
+++ service/crd/crd.go
@@ -2,25 +2,35 @@ package crd
 
 import (
 	"io/ioutil"
+	"strings"
 
 	"github.com/ghodss/yaml"
 	"github.com/giantswarm/microerror"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 )
 
-// Read reads a CRD YAML file and returns the Custom Resource Definition object it represents.
-func Read(inputFile string) (*apiextensionsv1.CustomResourceDefinition, error) {
-	crd := &apiextensionsv1.CustomResourceDefinition{}
-
-	yamlBytes, err := ioutil.ReadFile(inputFile)
+// Read reads a CRD YAML file and returns the Custom Resource Definition objects it represents.
+func Read(filePath string) ([]apiextensionsv1.CustomResourceDefinition, error) {
+	yamlBytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, microerror.Maskf(CouldNotReadCRDFileError, err.Error())
 	}
 
-	err = yaml.Unmarshal(yamlBytes, crd)
-	if err != nil {
-		return nil, microerror.Maskf(CouldNotParseCRDFileError, err.Error())
+	// Split by "---"
+	parts := strings.Split(string(yamlBytes), "\n---\n")
+	crds := []apiextensionsv1.CustomResourceDefinition{}
+
+	for _, crdYAMLString := range parts {
+		crdYAMLBytes := []byte(crdYAMLString)
+		crd := apiextensionsv1.CustomResourceDefinition{}
+
+		err = yaml.Unmarshal(crdYAMLBytes, &crd)
+		if err != nil {
+			return nil, microerror.Maskf(CouldNotParseCRDFileError, err.Error())
+		}
+
+		crds = append(crds, crd)
 	}
 
-	return crd, nil
+	return crds, nil
 }
