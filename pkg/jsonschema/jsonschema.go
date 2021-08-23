135580e93b5809335ab81fd0f43c6c503c5e40c7
Marian Steinbach <marian@giantswarm.io>
2021-08-23 10:24:16 +0200

    Move JSON schema handling into extra pkg

---
+++ pkg/jsonschema/jsonschema.go
@@ -0,0 +1,83 @@
+package jsonschema
+
+import (
+	"fmt"
+	"sort"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+// Property is a simplistic, flattened representation of a property
+// in a JSON Schema, without the recursion and containing only the elements
+// we intend to expose in our output.
+type Property struct {
+	// The depth of the item in the JSONPath hierarchy
+	Depth int8
+	// Path is the full JSONpath path of the attribute, e. g. ".spec.version".
+	Path string
+	// Name is the attribute name.
+	Name string
+	// Type is the textual representaiton of the type ("object", "array", "number", "string", "boolean").
+	Type string
+	// Description is a user-friendly description of the attribute.
+	Description string
+	// Required specifies whether the property is required.
+	Required bool
+}
+
+// Flatten recurses over all properties of a JSON Schema
+// and returns a flat slice of the elements we need for our output.
+func Flatten(schema *apiextensionsv1.JSONSchemaProps, properties []Property, depth int8, pathPrefix string) []Property {
+	// Capture names of required properties.
+	requiredProps := make(map[string]bool)
+	for _, p := range schema.Required {
+		requiredProps[p] = true
+	}
+
+	// Collect reduced property info.
+	for propname, schemaProps := range schema.Properties {
+		path := fmt.Sprintf("%s.%s", pathPrefix, propname)
+
+		required := false
+		if _, ok := requiredProps[propname]; ok {
+			required = true
+		}
+
+		property := Property{
+			Depth:       depth,
+			Name:        propname,
+			Path:        path,
+			Description: schemaProps.Description,
+			Type:        schemaProps.Type,
+			Required:    required,
+		}
+
+		properties = append(properties, property)
+
+		if len(schemaProps.Properties) > 0 {
+			properties = Flatten(&schemaProps, properties, depth+1, path)
+		}
+
+		if schemaProps.Type == "array" && schemaProps.Items != nil {
+			// Add description of array member type
+			property := Property{
+				Depth:       depth + 1,
+				Name:        propname + "[*]",
+				Path:        path + "[*]",
+				Description: schemaProps.Items.Schema.Description,
+				Type:        schemaProps.Items.Schema.Type,
+			}
+			properties = append(properties, property)
+
+			// Collect sub items properties
+			properties = Flatten(schemaProps.Items.Schema, properties, depth+2, path+"[*]")
+		}
+	}
+
+	// Sort properties by path.
+	sort.Slice(properties, func(i, j int) bool {
+		return properties[i].Path < properties[j].Path
+	})
+
+	return properties
+}
