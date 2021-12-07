c9c5d29d004c9548c40595c6c714f654ab10cb47
Marian Steinbach <marian@giantswarm.io>
2021-12-07 15:08:02 +0100

    Refactor config package to support multiple source repos

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,17 +9,36 @@ import (
 
 // FromFile represent a config file content.
 type FromFile struct {
-	SourceRepository *FromFileSourceRepository `yaml:"source_repository"`
-	TemplatePath     string                    `yaml:"template_path"`
+	SourceRepositories []SourceRepository `yaml:"source_repositories"`
+	TemplatePath       string             `yaml:"template_path"`
 }
 
-// FromFileSourceRepository has details about the
-// source repository to use for CRDs.
-type FromFileSourceRepository struct {
-	URL          string `yaml:"url"`
-	Organization string `yaml:"organization"`
-	ShortName    string `yaml:"short_name"`
-	MetadataPath string `yaml:"metadata_path"`
+// SourceRepository has details about a
+// source repository to find CRDs in.
+type SourceRepository struct {
+	URL             string             `yaml:"url"`
+	Organization    string             `yaml:"organization"`
+	ShortName       string             `yaml:"short_name"`
+	Metadata        map[string]CRDItem `yaml:"metadata"`
+	CommitReference string             `yaml:"commit_reference"`
+}
+
+type CRDItem struct {
+	Owners      []string     `yaml:"owner,omitempty"`
+	Topics      []string     `yaml:"topics,omitempty"`
+	Providers   []string     `yaml:"provider,omitempty"`
+	Hidden      bool         `yaml:"hidden,omitempty"`
+	Deprecation *Deprecation `yaml:"deprecation,omitempty"`
+}
+
+type Deprecation struct {
+	Info       string                 `yaml:"info,omitempty"`
+	ReplacedBy *DeprecationReplacedBy `yaml:"replaced_by,omitempty"`
+}
+
+type DeprecationReplacedBy struct {
+	FullName  string `yaml:"full_name"`
+	ShortName string `yaml:"short_name"`
 }
 
 // Read reads a config file and returns a struct.
