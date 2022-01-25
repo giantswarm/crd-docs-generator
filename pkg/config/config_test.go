1c00c885bcaa183ac27d34c3414caef08116658d
Marian Steinbach <marian@giantswarm.io>
2022-01-25 17:48:59 +0100

    Add annotations path to config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -23,11 +23,12 @@ func TestRead(t *testing.T) {
 			want: &FromFile{
 				SourceRepositories: []SourceRepository{
 					{
-						URL:          "https://github.com/giantswarm/apiextensions",
-						Organization: "giantswarm",
-						ShortName:    "apiextensions",
-						CRDPaths:     []string{"config/crd", "helm"},
-						CRPaths:      []string{"docs/cr"},
+						URL:             "https://github.com/giantswarm/apiextensions",
+						Organization:    "giantswarm",
+						ShortName:       "apiextensions",
+						AnnotationsPath: []string{"pkg/annotations"},
+						CRDPaths:        []string{"config/crd", "helm"},
+						CRPaths:         []string{"docs/cr"},
 						Metadata: map[string]CRDItem{
 							"crd.with.full.info": {
 								Owners:    []string{"owner"},
